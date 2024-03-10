8922bde1d4563e64a20ab42faabe86c855c375ec
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2024-03-10 05:42:20 +0000

    fix new whiteboard link

--- public/js/room.js
+++ public/js/room.js
@@ -718,7 +718,7 @@ whiteboardButt.addEventListener('click', () => {
     //     whiteboardCont.style.visibility = 'visible';
     //     boardVisisble = true;
     // }
-    window.open("https://gmeet-mkr9.onrender.com/", "_blank");
+    window.open("https://trex-whiteboard.netlify.app/65ub96bngpzbjj05eojv", "_blank");
 })
 
 cutCall.addEventListener('click', () => {
